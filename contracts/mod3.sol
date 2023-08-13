618ebc45e68d6c2bef1fa283cba879a9d1fb4a93
Aishu-ladha <100275145+Aishu-ladha@users.noreply.github.com>
2023-08-13 18:09:01 +0530

    Update mod3.sol

--- contracts/mod3.sol
+++ contracts/mod3.sol
@@ -1,42 +1,46 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
-/*
-       REQUIREMENTS
-    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
-    2. Your contract will have a mapping of addresses to balances (address => uint)
-    3. You will have a mint function that takes two parameters: an address and a value. 
-       The function then increases the total supply by that number and increases the balance 
-       of the “sender” address by that amount
-    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
-       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
-       and from the balance of the “sender”.
-    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
-       to the amount that is supposed to be burned.
-*/
-contract  MyToken {
-// public variables here
+
+contract MyToken {
+    // public variables here
     string public tokenName = "Aish";
     string public tokenAbbrv = "Meta";
     uint public totalSupply = 0;
-//mapping variables here
+    
+    // Owner address
+    address public owner;
+
+    //mapping variables here
     mapping (address => uint) public balances;
 
-//mint function code here increases balances
- function mint (address _address, uint _value) public{
-   totalSupply += _value;
-   balances[_address] += _value;
-}
-//burn function code here opposite of mint function
-function burn (address _address, uint _value) public{
-    if(balances[_address] >= _value){
-       totalSupply -= _value;
-       balances[_address] -= _value;
+    constructor() {
+        owner = msg.sender; // Set the contract deployer as the owner
     }
-  }
-  function transfer(address to, uint256 amount) public {
+    
+    // Modifier that only allows the owner to perform the action
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    //mint function code here increases balances
+    function mint(address _address, uint _value) public onlyOwner {
+        totalSupply += _value;
+        balances[_address] += _value;
+    }
+
+    //burn function code here opposite of mint function
+    function burn(address _address, uint _value) public onlyOwner {
+        require(balances[_address] >= _value, "Insufficient balance");
+
+        totalSupply -= _value;
+        balances[_address] -= _value;
+    }
+
+    function transfer(address to, uint256 amount) public {
         require(to != address(0), "Invalid address");
         require(amount > 0, "Amount must be greater than 0");
         require(balances[msg.sender] >= amount, "Insufficient balance");
